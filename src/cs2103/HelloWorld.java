2e02565ff1905e134a13f32af34a3a772e8febe2
zhenyu <devil_891@hotmail.com>
2014-08-20 15:00:56 +0800

    added main

--- src/cs2103/HelloWorld.java
+++ src/cs2103/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public HelloWorld() {
 		// TODO Auto-generated constructor stub
+		
+	}
+	public static void main(String[] args){
 		System.out.println("I am zhenyu");
 	}
 
