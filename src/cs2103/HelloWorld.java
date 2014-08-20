5f3799b97e7ae070912d98da28895a0d61d1043f
thida <thidawin007@gmail.com>
2014-08-20 15:18:30 +0800

    testing sync
    testing sync

--- src/cs2103/HelloWorld.java
+++ src/cs2103/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("I am zhenyu");
 		System.out.println("11/99 = 0.11111111111");
+		System.out.println("testing.");
 	}
 		
 
