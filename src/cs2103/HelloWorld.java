292f6b4383c8e5c44c337a57d0b904854230bc37
weijielim <waikit.limweijie@gmail.com>
2014-08-20 15:13:39 +0800

    added println
    1 line

--- src/cs2103/HelloWorld.java
+++ src/cs2103/HelloWorld.java
@@ -1,9 +1,14 @@
 package cs2103;
+import java.util.*;
 
 public class HelloWorld {
 
 	public HelloWorld() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("11/99 = 0.11111111111");
 	}
 
 }
