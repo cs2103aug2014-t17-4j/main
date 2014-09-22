a33fc44b24231b95956454e925919ea51ec68b3a
zhenyu <devil_891@hotmail.com>
2014-09-22 15:03:35 +0800

    added GUI controller

---
+++ task_catalyst/src/main/java/moustachio/task_catalyst/GraphicalInterfaceController.java
@@ -0,0 +1,125 @@
+package moustachio.task_catalyst;
+
+import java.util.List;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+
+public class GraphicalInterfaceController {
+	@FXML
+	private BorderPane rootBorderPane;
+	@FXML
+	private TableView<Task> taskTable;
+	@FXML
+	private TableColumn<Task, Integer> idColumn;
+	@FXML
+	private TableColumn<Task, String> taskColumn;
+	@FXML
+	private ListView<String> hashTagList;
+	@FXML
+	private Label statusMessage;
+	@FXML
+	private TextField commandBar;
+	@FXML
+	private Button settingsButton;
+	@FXML
+	private Button exitButton;
+
+	private static LogicActual logic = new LogicActual();
+
+	private static ObservableList<Task> taskToBeDisplayed = FXCollections
+			.observableArrayList();
+	private static ObservableList<String> hashTagToBeDisplayed = FXCollections
+			.observableArrayList();
+
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+		testInterface();
+		initializeForms();
+	}
+
+	@FXML
+	private void exitButtonAction() {
+		Platform.exit();
+	}
+
+	private void initializeForms() {
+		displayTask();
+		displayHashTags();
+	}
+
+	public void handleTextFieldOnAction(ActionEvent event) {
+		// TextField entered was clicked, do something...
+		Message message = logic.processCommand(commandBar.getText());
+		statusMessage.setText(message.getMessage());
+		clearForm();
+	}
+
+	public void handleTextFieldWhileUserTyping() {
+		// TextField entered was clicked, do something...
+		// TODO: Haven figure out which event handler
+		Message message = logic.processCommand(commandBar.getText());
+		statusMessage.setText(message.getMessage());
+	}
+
+	private void displayHashTags() {
+		hashTagList.setItems(getHashTagFromList());
+	}
+
+	public void clearForm() {
+		commandBar.clear();
+	}
+
+	private void displayTask() {
+		idColumn.setCellValueFactory(new PropertyValueFactory<Task, Integer>(
+				"id"));
+		taskColumn.setCellValueFactory(new PropertyValueFactory<Task, String>(
+				"description"));
+		taskTable.setItems(getTaskFromList());
+	}
+
+	private ObservableList<Task> getTaskFromList() {
+		List<Task> task = logic.getList();
+
+		taskToBeDisplayed.addAll(task);
+		return taskToBeDisplayed;
+	}
+
+	private ObservableList<String> getHashTagFromList() {
+		List<String> DefaultHashTags = logic.getDefaultHashtags();
+		List<String> hashTags = logic.getDefaultHashtags();
+
+		hashTagToBeDisplayed.addAll(DefaultHashTags);
+		hashTagToBeDisplayed.addAll(hashTags);
+
+		return hashTagToBeDisplayed;
+	}
+
+	private void testInterface() {
+		assert rootBorderPane != null : "fx:id=\"rootBorderPane\" was not injected: check your FXML file 'interface.fxml'.";
+		assert taskTable != null : "fx:id=\"taskTable\" was not injected: check your FXML file 'interface.fxml'.";
+		assert idColumn != null : "fx:id=\"idColumn\" was not injected: check your FXML file 'interface.fxml'.";
+		assert taskColumn != null : "fx:id=\"taskColumn\" was not injected: check your FXML file 'interface.fxml'.";
+		assert hashTagList != null : "fx:id=\"hashTagList\" was not injected: check your FXML file 'interface.fxml'.";
+		assert statusMessage != null : "fx:id=\"statusMessage\" was not injected: check your FXML file 'interface.fxml'.";
+		assert commandBar != null : "fx:id=\"commandBar\" was not injected: check your FXML file 'interface.fxml'.";
+		assert settingsButton != null : "fx:id=\"settingsButton\" was not injected: check your FXML file 'interface.fxml'.";
+		assert exitButton != null : "fx:id=\"exitButton\" was not injected: check your FXML file 'interface.fxml'.";
+	}
+
+}
