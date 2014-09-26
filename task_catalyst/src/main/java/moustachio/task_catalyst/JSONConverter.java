468c6ab84946ea27238866b1f9317f21c421ceab
thida <thidawin007@gmail.com>
2014-09-26 20:09:48 +0800

    add JSONConverter to use in Storage

---
+++ task_catalyst/src/main/java/moustachio/task_catalyst/JSONConverter.java
@@ -0,0 +1,60 @@
+package moustachio.task_catalyst;
+
+import moustachio.task_catalyst.TaskBasic;
+import java.time.LocalDateTime;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class JSONConverter {
+	
+	private JSONObject object;
+	
+	public JSONConverter(){
+		object = new JSONObject();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public JSONObject encode (Task task){
+		String dateStart = null;
+		String dateEnd= null;
+		if(task.getDateStart()==null);
+		else{
+			dateStart = task.getDateStart().toString();
+		}
+		if(task.getDateEnd()==null);
+		else{
+			dateEnd = task.getDateEnd().toString();
+		}
+		object.put("dateStart", dateStart);
+		object.put("dateEnd", dateEnd);
+		object.put("description", task.getDescription());
+		
+		return object;	
+	}
+	
+	public Task decode (JSONObject obj){
+		Task task = new TaskBasic("");
+		if (obj.get("description") != null) {
+			task.setDescription((String) obj.get("description"));
+		}
+		if(obj.get("startDateTime") == null){
+			task.setDateStart(null);
+		}
+		else{
+			task.setDateStart(LocalDateTime.parse((String)obj.get("startDate")));
+		}
+		if(obj.get("endDateTime")==null){
+			task.setDateEnd(null);
+		}
+		else{
+			task.setDateEnd(LocalDateTime.parse((String)obj.get("endDate")));
+		}
+		return task;
+	}
+
+	public Task decodeToStr(String str) throws ParseException{
+		JSONObject obj = (JSONObject) JSONValue.parseWithException(str);
+		return decode(obj);
+	}
+}
