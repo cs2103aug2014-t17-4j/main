24b81957972004602533b855635c21ccf2ea17a6
thida <thidawin007@gmail.com>
2014-09-29 20:44:16 +0800

    remove uncessary import java

--- task_catalyst/src/main/java/moustachio/task_catalyst/FileHandler.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/FileHandler.java
@@ -2,12 +2,10 @@ package moustachio.task_catalyst;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
