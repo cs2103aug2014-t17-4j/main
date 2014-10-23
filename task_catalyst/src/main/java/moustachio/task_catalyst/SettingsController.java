c4a9a946cfba4c1425038ee606d33ac248e9c40f
zhenyu <devil_891@hotmail.com>
2014-10-23 11:23:52 +0800

    Added Settings Controller & Settings GUI

---
+++ task_catalyst/src/main/java/moustachio/task_catalyst/SettingsController.java
@@ -0,0 +1,32 @@
+package moustachio.task_catalyst;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class SettingsController {
+	@FXML
+	private BorderPane rootBorderPane;
+	@FXML
+	private Button exitButton;
+	@FXML
+	private Button saveButton;
+	@FXML
+	private Button cancelButton;
+
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+		testInterface();
+	}
+
+	private void testInterface() {
+		assert rootBorderPane != null : "fx:id=\"rootBorderPane\" was not injected: check your FXML file 'settings.fxml'.";
+		assert exitButton != null : "fx:id=\"exitButton\" was not injected: check your FXML file 'settings.fxml'.";
+		assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'settings.fxml'.";
+		assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'settings.fxml'.";
+	}
+}
