c9daacffabc7e0b267d975f5d83269030117bd21
zhenyu <devil_891@hotmail.com>
2014-10-23 11:30:23 +0800

    1) Renamed GUIController to UIController 2) Added a function to open Settings window @ line 84 3) Added Initial highlighting of the Hashtags 4) Added functions to handle highlighting of Task table (setFocusForTaskTable)& Hashtag table (setFocusForHashTable) 5) Added multiple selection in Task table 6) Added warped text for Task Table

---
+++ task_catalyst/src/main/java/moustachio/task_catalyst/UIController.java
@@ -0,0 +1,264 @@
+package moustachio.task_catalyst;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableView.TableViewFocusModel;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+public class UIController {
+	@FXML
+	private BorderPane rootBorderPane;
+	@FXML
+	private ImageView programTitle;
+	@FXML
+	private TableView<Task> taskTable;
+	@FXML
+	private TableColumn<Task, Integer> idColumn;
+	@FXML
+	private TableColumn<Task, String> taskColumn;
+	@FXML
+	private ListView<String> hashTagList;
+	@FXML
+	private Label statusMessage;
+	@FXML
+	private TextField commandBar;
+	@FXML
+	private Button settingsButton;
+	@FXML
+	private Button exitButton;
+
+	private static LogicActual logic;
+
+	private static ObservableList<Task> taskToBeDisplayed = FXCollections
+			.observableArrayList();
+	private static ObservableList<String> hashTagToBeDisplayed = FXCollections
+			.observableArrayList();
+
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+		logic = new LogicActual();
+		testInterface();
+		initializeForms();
+	}
+
+	@FXML
+	private void exitButtonAction() {
+		Platform.exit();
+	}
+
+	@FXML
+	private void openSettingsWindow() {
+		Pane myPane;
+		try {
+			myPane = FXMLLoader.load(getClass().getResource("settings.fxml"));
+			Stage stage = new Stage();
+			stage.setScene(new Scene(myPane));
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initializeForms() {
+		initializeTable();
+		statusMessage.setWrapText(true);
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				hashTagList.scrollTo(0);
+				hashTagList.getSelectionModel().select(0);
+				commandBar.requestFocus();
+			}
+		});
+		displayTask();
+		displayHashTags();
+	}
+
+	private void setFocusForTaskTable(int index) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				taskTable.scrollTo(index);
+				taskTable.getSelectionModel().select(index);
+			}
+		});
+	}
+
+	private void setFocusForHashTable(int index) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				hashTagList.scrollTo(index);
+				hashTagList.getSelectionModel().select(index);
+			}
+		});
+	}
+
+	private void initializeTable() {
+		// Enable multiple selection for the table
+		taskTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+		// Set message shown when there's no content
+		// taskTable.setPlaceholder();
+		// taskTable.setPlaceholder(new Text("Your localized text here"));
+	}
+
+	public void handleTextFieldOnAction(ActionEvent event) {
+		Message message = logic.processCommand(commandBar.getText());
+		switch (message.getType()) {
+		case Message.TYPE_SUCCESS:
+			statusMessage.setText(message.getMessage());
+
+			if (commandBar.getText().contains("edit")) {
+				String[] getIndexForFocus = commandBar.getText().split("\\s");
+				if (getIndexForFocus.length >= 1) {
+					setFocusForTaskTable(Integer.valueOf(getIndexForFocus[1]));
+				}
+			}
+
+			displayHashTags();
+			displayTask();
+			clearForm();
+			break;
+		case Message.TYPE_ERROR:
+			statusMessage.setText(message.getMessage());
+			break;
+		}
+	}
+
+	public void handleTextFieldWhileUserTyping(KeyEvent event) {
+		if (!event.getCode().equals(KeyCode.ENTER)) {
+			Message message = logic.getMessageTyping(commandBar.getText());
+
+			if (message.getType() == Message.TYPE_AUTOCOMPLETE) {
+				commandBar.setText(message.getMessage());
+				commandBar.positionCaret(commandBar.getText().length());
+				handleTextFieldWhileUserTyping(event);
+			} else {
+				statusMessage.setText(message.getMessage());
+			}
+		}
+	}
+
+	private void displayHashTags() {
+		hashTagList.setItems(getHashTagFromList());
+	}
+
+	private void clearForm() {
+		commandBar.clear();
+	}
+
+	private void displayTask() {
+		if (logic.getList() != null) {
+			idColumn.setCellValueFactory(new Callback<CellDataFeatures<Task, Integer>, ObservableValue<Integer>>() {
+				@Override
+				public ObservableValue<Integer> call(
+						CellDataFeatures<Task, Integer> p) {
+					return new ReadOnlyObjectWrapper<Integer>(
+							(Integer) taskTable.getItems()
+									.indexOf(p.getValue()) + 1);
+				}
+			});
+
+			taskColumn
+					.setCellValueFactory(new PropertyValueFactory<Task, String>(
+							"description"));
+
+			taskColumn
+					.setCellFactory(new Callback<TableColumn<Task, String>, TableCell<Task, String>>() {
+						@Override
+						public TableCell<Task, String> call(
+								TableColumn<Task, String> arg0) {
+							return new TableCell<Task, String>() {
+								private Text text;
+
+								@Override
+								public void updateItem(String item,
+										boolean empty) {
+									super.updateItem(item, empty);
+									if (!isEmpty()) {
+										text = new Text(item.toString());
+										text.setWrappingWidth(taskColumn
+												.getWidth());
+										this.setWrapText(true);
+
+										setGraphic(text);
+									}
+								}
+							};
+						}
+					});
+
+			idColumn.setSortable(false);
+			taskColumn.setSortable(false);
+			taskTable.setItems(getTaskFromList());
+		}
+	}
+
+	private ObservableList<Task> getTaskFromList() {
+		List<Task> task = logic.getList();
+
+		taskToBeDisplayed.clear();
+		taskToBeDisplayed.addAll(task);
+		return taskToBeDisplayed;
+	}
+
+	private ObservableList<String> getHashTagFromList() {
+		List<String> DefaultHashTags = logic.getDefaultHashtags();
+		List<String> hashTags = logic.getHashtags();
+
+		hashTagToBeDisplayed.clear();
+		hashTagToBeDisplayed.addAll(DefaultHashTags);
+		hashTagToBeDisplayed.addAll(hashTags);
+
+		return hashTagToBeDisplayed;
+	}
+
+	private void testInterface() {
+		assert rootBorderPane != null : "fx:id=\"rootBorderPane\" was not injected: check your FXML file 'interface.fxml'.";
+		assert programTitle != null : "fx:id=\"rootBorderPane\" was not injected: check your FXML file 'interface.fxml'.";
+		assert taskTable != null : "fx:id=\"taskTable\" was not injected: check your FXML file 'interface.fxml'.";
+		assert idColumn != null : "fx:id=\"idColumn\" was not injected: check your FXML file 'interface.fxml'.";
+		assert taskColumn != null : "fx:id=\"taskColumn\" was not injected: check your FXML file 'interface.fxml'.";
+		assert hashTagList != null : "fx:id=\"hashTagList\" was not injected: check your FXML file 'interface.fxml'.";
+		assert statusMessage != null : "fx:id=\"statusMessage\" was not injected: check your FXML file 'interface.fxml'.";
+		assert commandBar != null : "fx:id=\"commandBar\" was not injected: check your FXML file 'interface.fxml'.";
+		assert settingsButton != null : "fx:id=\"settingsButton\" was not injected: check your FXML file 'interface.fxml'.";
+		assert exitButton != null : "fx:id=\"exitButton\" was not injected: check your FXML file 'interface.fxml'.";
+	}
+
+}
