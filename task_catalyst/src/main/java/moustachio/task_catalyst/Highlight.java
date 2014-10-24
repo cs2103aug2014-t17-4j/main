ba633907e57e9deca20d467d41d0c20de6451446
Syraxius <syraxius@hotmail.com>
2014-10-24 22:41:54 +0800

    Highlight Class

--- task_catalyst/src/main/java/moustachio/task_catalyst/Highlight.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/Highlight.java
@@ -1,21 +1,26 @@
 package moustachio.task_catalyst;
 
 public class Highlight {
-	public static final int TYPE_HASHTAG = 0;
-	public static final int TYPE_LAST_ADDED = 1;
-	public static final int TYPE_LAST_RESTORED = 2;
-	public static final int TYPE_OVERLAP_STATIC = 3;
-	public static final int TYPE_OVERLAP_DYNAMIC = 4;
+	public static final int TYPE_HASHTAG_DEFAULT = 0;
+	public static final int TYPE_HASHTAG_CUSTOM = 1;
+	public static final int TYPE_HASHTAG_INVALID = 2;
+	public static final int TYPE_SEARCH = 3;
+	public static final int TYPE_TASK_LAST_ADDED = 4;
+	public static final int TYPE_TASK_OVERLAP_STATIC = 5;
+	public static final int TYPE_TASK_OVERLAP_DYNAMIC = 6;
+	public static final int TYPE_TASK_PRIORITY = 7;
+	public static final int TYPE_TASK_PRIORITY_OVERLAP_STATIC = 8;
+	public static final int TYPE_TASK_PRIORITY_OVERLAP_DYNAMIC = 9;
 
 	private int taskIndex;
 	private int highlightType;
 
-	public Highlight(int taskIndex, int highlightType) {
+	public Highlight(int highlightType, int taskIndex) {
 		this.taskIndex = taskIndex;
 		this.highlightType = highlightType;
 	}
 
-	public int getTaskIndex() {
+	public int getIndex() {
 		return taskIndex;
 	}
 
