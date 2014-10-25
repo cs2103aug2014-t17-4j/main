49a409b5ac46e360fe0c5a69d9d0fa8d634251a4
weijielim <weijielim@users.noreply.github.com>
2014-10-26 00:14:51 +0800

    Added overlap function in ListProcessor

--- task_catalyst/src/main/java/moustachio/task_catalyst/ListProcessorActual.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/ListProcessorActual.java
@@ -1,17 +1,16 @@
 package moustachio.task_catalyst;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class ListProcessorActual implements ListProcessor {
+	List<Task> filteredList = new ArrayList<Task>();
 	
 	@Override
 	public List<Task> searchByHashtag(List<Task> list, String hashtag) {
-		List<Task> filteredList = new ArrayList<Task>();
-		LocalDate today = LocalDate.now();
-		LocalDate tomorrow = today.plusDays(1);
 		switch(hashtag) {
 			case "all": 
 				for(Task task:list) {
@@ -22,22 +21,49 @@ public class ListProcessorActual implements ListProcessor {
 				return filteredList;
 			case "tdy": 
 				for(Task task:list) {
-					if(task.getDateEnd() != null && task.getDateEnd().toLocalDate().isEqual(today) && !task.isDone()) {
+					if(!task.getAllDates().isEmpty() && task.isRange() 
+						&& (TaskCatalystCommons.daysFromToday(task.getDateEnd()) == 0)
+						&& !task.isDone()) {
 						filteredList.add(task);
+					}
+					else if(!task.getAllDates().isEmpty() && !task.isRange() && !task.isDone()) {
+						for(Date date:task.getAllDates()) {
+							if(TaskCatalystCommons.isToday(date)) {
+								filteredList.add(task);
+							}
+						}
 					}
 				}
 				return filteredList;
 			case "tmr": 
 				for(Task task:list) {
-					if(task.getDateEnd() != null && task.getDateEnd().toLocalDate().isEqual(tomorrow) && !task.isDone()) {
+					if(!task.getAllDates().isEmpty() && task.isRange() 
+						&& (TaskCatalystCommons.daysFromToday(task.getDateEnd()) == 1)
+						&& !task.isDone()) {
 						filteredList.add(task);
+					}
+					else if(!task.getAllDates().isEmpty() && !task.isRange() && !task.isDone()) {
+						for(Date date:task.getAllDates()) {
+							if(TaskCatalystCommons.isTomorrow(date)) {
+								filteredList.add(task);
+							}
+						}
 					}
 				}
 				return filteredList;
 			case "upc": 
 				for(Task task:list) {
-					if(task.getDateEnd() != null && task.getDateEnd().toLocalDate().isAfter(tomorrow) && !task.isDone()) {
+					if(!task.getAllDates().isEmpty() && task.isRange() 
+						&& (TaskCatalystCommons.daysFromToday(task.getDateEnd()) >= 2)
+						&& !task.isDone()) {
 						filteredList.add(task);
+					}
+					else if(!task.getAllDates().isEmpty() && !task.isRange() && !task.isDone()) {
+						for(Date date:task.getAllDates()) {
+							if(TaskCatalystCommons.daysFromToday(date) >= 2) {
+								filteredList.add(task);
+							}
+						}
 					}
 				}
 				return filteredList;
@@ -67,32 +93,88 @@ public class ListProcessorActual implements ListProcessor {
 
 	@Override
 	public List<Task> searchByKeyword(List<Task> list, String keyword) {
-		List<Task> searchList = new ArrayList<Task>();
 		for(Task task:list) {
 			if(task.hasKeyword(keyword)) {
-				searchList.add(task);
+				filteredList.add(task);
 			}
 		}
-		return searchList;
+		return filteredList;
 	}
 
 	@Override
 	public List<Task> sortByDate(List<Task> list) {
-		List<Task> sortList = new ArrayList<Task>(list);
-		Collections.sort(sortList);
-		return sortList;
+		Collections.sort(filteredList);
+		return filteredList;
 	}
 
 	@Override
 	public List<Task> getOverlapping(List<Task> list) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Task> overlapList = new ArrayList<Task>();
+		for(Task task:list) {
+			List<Task> tempList = new ArrayList<Task>(getOverlapping(task,list));
+			if(!tempList.isEmpty()) {
+				overlapList.add(task);
+				overlapList.addAll(tempList);
+			}
+		}
+		return new ArrayList<Task>(new LinkedHashSet<Task>(overlapList));
 	}
 
 	@Override
 	public List<Task> getOverlapping(Task task, List<Task> list) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Task> overlapList = new ArrayList<Task>();
+		for(Task task2:list) {
+			if(isOverlap(task, task2)) {
+				overlapList.add(task2);
+			}
+		}
+		return overlapList;
+	}
+	
+	private boolean isOverlap(Task task1, Task task2) {
+		List<Date> list1 = new ArrayList<Date>(task1.getAllDates());
+		List<Date> list2 = new ArrayList<Date>(task2.getAllDates());
+		if(list1.isEmpty()|| list2.isEmpty() || task1.equals(task2) || task1.isDone() || task2.isDone()) {
+			return false;
+		}
+		if(task1.isRange() && task2.isRange()) {
+			if(task1.getDateEnd().before(task2.getDateStart()) || task2.getDateEnd().before(task1.getDateStart())) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		else if(task1.isRange() && !task2.isRange()) {
+			for(Date date:list2) {
+				if((date.before(task1.getDateEnd()) && date.after(task1.getDateStart())) 
+					|| date.equals(task1.getDateStart()) 
+					|| date.equals(task1.getDateEnd())) {
+					return true;
+				}
+			}
+			return false;
+		}
+		else if(!task1.isRange() && task2.isRange()) {
+			for(Date date:list1) {
+				if((date.before(task2.getDateEnd()) && date.after(task2.getDateStart()))
+					|| date.equals(task2.getDateStart()) 
+					|| date.equals(task2.getDateEnd())){
+					return true;
+				}
+			}
+			return false;
+		}
+		else {
+			for(Date date1:list1) {
+				for(Date date2:list2) {
+					if(TaskCatalystCommons.isSameDate(date1, date2) && TaskCatalystCommons.isSameTime(date1, date2)) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
 	}
 
 }
