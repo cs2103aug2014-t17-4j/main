3392a141a58b516025334f94056418a672fd266a
thida <thidawin007@gmail.com>
2014-10-28 18:21:53 +0800

    added system exit when user type exit in command bar

--- task_catalyst/src/main/java/moustachio/task_catalyst/UIController.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/UIController.java
@@ -181,6 +181,11 @@ public class UIController {
 					setFocusForTaskTable(Integer.valueOf(getIndexForFocus[1]));
 				}
 			}
+			
+			if(commandBar.getText().trim().equalsIgnoreCase("exit")){
+				this.tc.stop();
+			}
+			
 			setFocusForHashTable(logic.getHashtagSelected());
 			setFocusForTaskTableList(logic.getTasksSelected());
 			displayHashTags();
