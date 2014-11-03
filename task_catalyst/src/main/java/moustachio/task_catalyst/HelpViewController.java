019d656b805633f067ae9fa27662a2b0f256a0c7
thida <thidawin007@gmail.com>
2014-11-03 23:08:39 +0800

    added draglister and mouse dragged

--- task_catalyst/src/main/java/moustachio/task_catalyst/HelpViewController.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/HelpViewController.java
@@ -4,14 +4,18 @@ package moustachio.task_catalyst;
 import java.io.IOException;
 
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -45,6 +49,9 @@ public class HelpViewController {
 	private static final String QUICKGUIDE_FXML_PATH = "/fxml/quickGuide.fxml";
 	
 	private HelpViewController controller;
+	
+	private double initialY;
+	private double initialX;
 
 	public HelpViewController() {
 		
@@ -80,6 +87,37 @@ public class HelpViewController {
 		});
 	}
 	
+	private void addDragListeners(final Node helpUI) {
+
+		helpUI.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent me) {
+
+				if (me.getButton() != MouseButton.MIDDLE) {
+					initialX = me.getSceneX();
+					initialY = me.getSceneY();
+				} else {
+					helpUI.getScene().getWindow().centerOnScreen();
+					initialX = helpUI.getScene().getWindow().getX();
+					initialY = helpUI.getScene().getWindow().getY();
+				}
+
+			}
+		});
+
+		helpUI.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent me) {
+				if (me.getButton() != MouseButton.MIDDLE) {
+					helpUI.getScene().getWindow()
+							.setX(me.getScreenX() - initialX);
+					helpUI.getScene().getWindow()
+							.setY(me.getScreenY() - initialY);
+				}
+			}
+		});
+	}
+	
 	public void openHelpWindow() {
 		AnchorPane helpPane;
 		if (helpStage != null) {
@@ -94,6 +132,7 @@ public class HelpViewController {
 				helpPane = loader.load();
 				
 				controller = loader.getController();
+				addDragListeners(helpPane);
 				controller.setStage(helpStage);
 				helpStage.setScene(new Scene(helpPane));
 				helpStage.initStyle(StageStyle.UNDECORATED);
