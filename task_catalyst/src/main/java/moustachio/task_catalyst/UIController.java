2ab4a3bbb5219649f2bef70bd5dd157f0f8f53b6
zhenyu <devil_891@hotmail.com>
2014-11-04 13:54:50 +0800

    Centered empty message in the middle of scrollpane

--- task_catalyst/src/main/java/moustachio/task_catalyst/UIController.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/UIController.java
@@ -17,6 +17,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -26,8 +28,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -359,8 +363,12 @@ public class UIController {
 		// taskContainer.getStyleClass().add("vbox");
 
 		if (task.isEmpty()) {
-			taskContainer.getChildren().add(new Label(EMPTY_TASKVIEW_MESSAGE));
-			taskScrollPane.setContent(taskContainer);
+			StackPane container = new StackPane();
+			Label messageLabel = new Label(EMPTY_TASKVIEW_MESSAGE); 
+			messageLabel.setPrefHeight(taskScrollPane.getHeight());
+			container.getChildren().add(messageLabel);
+			container.setAlignment(Pos.CENTER);
+			taskScrollPane.setContent(container);
 		} else {
 			for (int i = 0; i < task.size(); i++) {
 				Task currentTask = task.get(i);
