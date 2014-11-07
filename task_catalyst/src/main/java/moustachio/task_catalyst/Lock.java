722b828d795ba23530c10c35995594a3713dbdbc
thida <thidawin007@gmail.com>
2014-11-07 23:25:49 +0800

    refractored

--- task_catalyst/src/main/java/moustachio/task_catalyst/Lock.java
+++ task_catalyst/src/main/java/moustachio/task_catalyst/Lock.java
@@ -2,6 +2,7 @@ package moustachio.task_catalyst;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.util.logging.Level;
@@ -11,19 +12,28 @@ import java.util.logging.Logger;
  * This class is to create lock for global hotkeys. 
  * 
  * @author A0112764J -reused
+ * 
+ * This was adapted from a post from Rumato Estorsky posted 20th July 2012 on following blog:
+ * 
+ * @url http://nerdydevel.blogspot.com/2012/07/run-only-single-java-application-instance.html
  *
  */
 
 public class Lock {
+	private static final String EXTENSION_APP_LOCK = ".app_lock";
+	private static final String EXTIONSION_LOCK = ".lock";
+	
 	private static final String MESSAGE_INSTATCE_IS_NULL = "INSTATCE IS NULL!";
 	private static final String MESSAGE_FAIL_TO_SET_LOCK = "Fail to set Lock.";
 	private static final String MESSAGE_FAIL_TO_CREATE_LOCK = "Can't create Lock";
 	private static final String MESSAGE_ANOTHER_INSTANCE_DETECTED = "Another instance detected";
 	private static final String MESSAGE_LOCKED_BY_KEY = "Java Lock Object\r\nLocked by key: ";
-	private static final String MD5 = "MD5";
+	private static final String MESSAGE_LOCK_FILE_FAIL = "Lock.Lock() file fail";
+	
+	private static final String NAME_LOCK_FILE = "MD5";
 	private static final String FILE_SEPARATOR = "file.separator";
 	private static final String JAVA_IO_TMPDIR = "java.io.tmpdir";
-	private static final String MESSAGE_LOCK_FILE_FAIL = "Lock.Lock() file fail";
+
 
 	private Lock() {
 	}
@@ -43,29 +53,38 @@ public class Lock {
 	 * @throws Exception
 	 */
 	private Lock(String lockKey) throws Exception {
+		String tmpDir = setTempDir();
+		// Acquire MD5 - lock file
+		acquireMD5(lockKey, tmpDir);
+		// MD5 acquire fail
+		if (lockFile == null) {
+			createLockFile(lockKey, tmpDir, EXTIONSION_LOCK);
+		}
+		lockStream = new FileOutputStream(lockFile);
+		writeLockStream(MESSAGE_LOCKED_BY_KEY + lockKey
+				+ "\r\n");
+		createLock();
+		printFailureMessage();
+	}
+
+	private void createLockFile(String lockKey, String tmpDir, String extension) {
+		lockFile = new File(tmpDir + lockKey + extension);
+	}
+
+	private void createLock() throws IOException {
+		lockChannel = lockStream.getChannel();
+		lock = lockChannel.tryLock();
+	}
+
+	private String setTempDir() {
 		String tmpDir = System.getProperty(JAVA_IO_TMPDIR);
 		if (!tmpDir.endsWith(System.getProperty(FILE_SEPARATOR))) {
 			tmpDir += System.getProperty(FILE_SEPARATOR);
 		}
+		return tmpDir;
+	}
 
-		// Acquire MD5
-		acquireMD5(lockKey, tmpDir);
-
-		// MD5 acquire fail
-		if (lockFile == null) {
-			lockFile = new File(tmpDir + lockKey + ".lock");
-		}
-
-		lockStream = new FileOutputStream(lockFile);
-
-		String fileContent = MESSAGE_LOCKED_BY_KEY + lockKey
-				+ "\r\n";
-		writeLockStream(fileContent);
-
-		lockChannel = lockStream.getChannel();
-
-		lock = lockChannel.tryLock();
-
+	private void printFailureMessage() throws Exception {
 		if (lock == null) {
 			throw new Exception(MESSAGE_FAIL_TO_CREATE_LOCK);
 		}
@@ -74,16 +93,16 @@ public class Lock {
 	private void acquireMD5(String lockKey, String tmpDir) {
 		try {
 			java.security.MessageDigest md = java.security.MessageDigest
-					.getInstance(MD5);
+					.getInstance(NAME_LOCK_FILE);
 			md.reset();
 			String hashText = new java.math.BigInteger(1, md.digest(lockKey
 					.getBytes())).toString(16);
 			// Hash string has no leading zeros
-			// Adding zeros to the beginnig of has string
+			// Adding zeros to the beginning of has string
 			while (hashText.length() < 32) {
 				hashText = "0" + hashText;
 			}
-			lockFile = new File(tmpDir + hashText + ".app_lock");
+			createLockFile(hashText,tmpDir,EXTENSION_APP_LOCK);
 		} catch (Exception ex) {
 			System.out.println(MESSAGE_LOCK_FILE_FAIL);
 		}
