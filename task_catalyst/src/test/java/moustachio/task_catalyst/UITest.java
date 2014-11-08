f1f81fde9cb051e451b4e3cb13fa2e57f557b19a
zhenyu <devil_891@hotmail.com>
2014-11-08 12:29:48 +0800

    added test case for gui and set padding for icons

--- task_catalyst/src/test/java/moustachio/task_catalyst/UITest.java
+++ task_catalyst/src/test/java/moustachio/task_catalyst/UITest.java
@@ -1,22 +1,28 @@
 package moustachio.task_catalyst;
 
+import static org.loadui.testfx.Assertions.verifyThat;
+import static org.loadui.testfx.controls.Commons.hasText;
+
 import java.io.IOException;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
 
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 
-import static org.loadui.testfx.Assertions.verifyThat;
-import static org.loadui.testfx.controls.Commons.hasText;
-import static org.loadui.testfx.controls.TableViews.containsCell;
-import static org.loadui.testfx.controls.TableViews.numberOfRowsIn;
-import javafx.scene.input.KeyCode;
+import javafx.scene.control.ListView;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.loadui.testfx.categories.TestFX;
+
+import static javafx.collections.FXCollections.observableArrayList;
+import static org.hamcrest.core.IsNot.not;
+import static org.loadui.testfx.controls.ListViews.containsRow;
+import static org.loadui.testfx.controls.ListViews.numberOfRowsIn;
 
 public class UITest extends GuiTest {
 	public Parent getRootNode() {
@@ -30,44 +36,59 @@ public class UITest extends GuiTest {
 		return root;
 	}
 
-	@Test
-	public void test() {
-		type("Meet boss at 1pm");
-		verifyThat(".text-field", hasText("Meet boss at 1pm"));
-	}
-
-	@Test
-	public void test2() {
-		type("Meet boss at 1pm");
+	private void deleteAllTask(){
+		type("del all");
 		push(KeyCode.ENTER);
-		verifyThat("#statusMessage",
-				hasText("Task successfully added: Meet boss today 1PM"));
 	}
-
-	// This is a partial command partition
+	
+	//check status message text
+	@Test
+	public void test1() {
+		verifyThat("#statusMessage",
+				hasText("Type something to begin adding a task.\nOther Commands: delete, edit, done, redo, undo, #, find. Press CTRL+H for more details."));
+	}
+	
+	//delete all task added previously
+	@Test
+    public void test2() {
+		deleteAllTask();
+		//check status message when deleting empty task list
+		deleteAllTask();
+        verifyThat("#statusMessage",
+				hasText("There was/were no matching task(s) to delete."));
+    }
+	
+	//test adding of floating task
 	@Test
 	public void test3() {
-		type("edi");
-		verifyThat("#statusMessage", hasText("Do you mean \"edit\"?"));
+		type("Floating task");
+		push(KeyCode.ENTER);
+		verifyThat("#statusMessage",
+				hasText("Task successfully added: Floating task"));
 	}
 
-	// This is a partial command partition
+	//test adding of single time task
 	@Test
 	public void test4() {
-		type("edit");
-		verifyThat(
-				"#statusMessage",
-				hasText("Edit: Hit space or enter after typing a valid task number to continue.\nSyntax: edit <task number>"));
-
-		type(" ");
-		verifyThat(
-				"#statusMessage",
-				hasText("Edit: Hit space or enter after typing a valid task number to continue.\nSyntax: edit <task number>"));
-
-		type("1");
-		verifyThat(
-				"#statusMessage",
-				hasText("Edit: Hit space or enter after typing a valid task number to continue.\nSyntax: edit <task number>"));
+		type("task 5pm");
+		push(KeyCode.ENTER);
+		verifyThat("#statusMessage",
+				hasText("Task successfully added: task today 5PM"));
 	}
 
+	//test adding of task with time range
+	@Test
+	public void test5() {
+		type("task from 2pm to 3pm");
+		push(KeyCode.ENTER);
+		verifyThat("#statusMessage", hasText("Task successfully added: task from today 2PM to 3PM"));
+	}
+
+	//test adding of task with reserved time slots
+	@Test
+	public void test6() {
+		type("task at 2pm or 3pm or 4pm");
+		push(KeyCode.ENTER);
+		verifyThat("#statusMessage", hasText("Task successfully added: task today 2PM, 3PM or 4PM"));
+	}
 }
