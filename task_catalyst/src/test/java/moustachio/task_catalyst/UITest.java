e96c324a9db176d569ed064808571f8870c68513
zhenyu <devil_891@hotmail.com>
2014-11-10 00:35:46 +0800

    fixed exception

--- task_catalyst/src/test/java/moustachio/task_catalyst/UITest.java
+++ task_catalyst/src/test/java/moustachio/task_catalyst/UITest.java
@@ -2,82 +2,86 @@ package moustachio.task_catalyst;
 
 import static org.loadui.testfx.Assertions.verifyThat;
 import static org.loadui.testfx.controls.Commons.hasText;
-
-import java.io.IOException;
-
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.input.KeyCode;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
+import org.loadui.testfx.utils.FXTestUtils;
 
-//@author A0111921W
-public class UITest extends GuiTest {
-	public Parent getRootNode() {
-		Parent root = null;
-		try {
-			root = FXMLLoader.load(getClass().getResource(
-					"/fxml/userInterface.fxml"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return root;
+//@author A0111921W - adapted from https://github.com/TestFX/TestFX/issues/57
+public class UITest {
+	private static GuiTest controller;
+	private static TaskCatalyst tc;
+
+	@BeforeClass
+	public static void setUpClass() {
+		FXTestUtils.launchApp(TaskCatalyst.class);
+
+		// here is that closure I talked about above, you instantiate the
+		// getRootNode abstract method
+		// which requires you to return a 'parent' object, luckily for us,
+		// getRoot() gives a parent!
+		// getRoot() is available from ALL Node objects, which makes it easy.
+		controller = new GuiTest() {
+			@Override
+			protected Parent getRootNode() {
+				return tc.getStage().getScene().getRoot();
+			}
+		};
+
 	}
 
-	private void deleteAllTask(){
-		type("del all");
-		push(KeyCode.ENTER);
+	private void deleteAllTask() {
+		controller.type("del all");
+		controller.push(KeyCode.ENTER);
 	}
-	
-	//check status message text
+
+	// test adding of floating task
 	@Test
 	public void test1() {
-		verifyThat("#statusMessage",
-				hasText("Type something to begin adding a task.\nOther Commands: delete, edit, done, restore, redo, undo, #, find. Press CTRL+H for more details."));
-	}
-	
-	//delete all task added previously
-	@Test
-    public void test2() {
-		deleteAllTask();
-		//check status message when deleting empty task list
-		deleteAllTask();
-        verifyThat("#statusMessage",
-				hasText("There was/were no matching task(s) to delete."));
-    }
-	
-	//test adding of floating task
-	@Test
-	public void test3() {
-		type("Floating task");
-		push(KeyCode.ENTER);
+		controller.type("Floating task");
+		controller.push(KeyCode.ENTER);
 		verifyThat("#statusMessage",
 				hasText("Task successfully added: Floating task"));
 	}
 
-	//test adding of single time task
+	// test adding of single time task
 	@Test
-	public void test4() {
-		type("task 5pm");
-		push(KeyCode.ENTER);
+	public void test2() {
+		controller.type("task 5pm");
+		controller.push(KeyCode.ENTER);
 		verifyThat("#statusMessage",
 				hasText("Task successfully added: task today 5PM"));
 	}
 
-	//test adding of task with time range
+	// test adding of task with time range
 	@Test
-	public void test5() {
-		type("task from 2pm to 3pm");
-		push(KeyCode.ENTER);
-		verifyThat("#statusMessage", hasText("Task successfully added: task from today 2PM to 3PM"));
+	public void test3() {
+		controller.type("task from 2pm to 3pm");
+		controller.push(KeyCode.ENTER);
+		verifyThat("#statusMessage",
+				hasText("Task successfully added: task from today 2PM to 3PM"));
 	}
 
-	//test adding of task with reserved time slots
+	// test adding of task with reserved time slots
 	@Test
-	public void test6() {
-		type("task at 2pm or 3pm or 4pm");
-		push(KeyCode.ENTER);
-		verifyThat("#statusMessage", hasText("Task successfully added: task today 2PM, 3PM or 4PM"));
+	public void test4() {
+		controller.type("task at 2pm or 3pm or 4pm");
+		controller.push(KeyCode.ENTER);
+		verifyThat("#statusMessage",
+				hasText("Task successfully added: task today 2PM, 3PM or 4PM"));
 	}
+
+	// delete all task added previously
+	@Test
+	public void test5() {
+		deleteAllTask();
+		// check status message when deleting empty task list
+		deleteAllTask();
+		verifyThat("#statusMessage",
+				hasText("There was/were no matching task(s) to delete."));
+	}
+	
 }
